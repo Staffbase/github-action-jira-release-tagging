8560069bf78e739da0e323a5bea3343f22b75eb9
Tim Kante <tim.kante@staffbase.com>
2020-03-05 21:02:26 +0100

    :bug: fixed tests

--- __tests__/action.test.js
+++ __tests__/action.test.js
@@ -1,7 +1,7 @@
 const action = require('../action')
 
 test('action#filterIssueIds empty', () => {
-  const result = action.filterIssueIds('PAC-000,RE-123,,LOL-333,PACMAN-123,DIABLO-456,PACMAN-000,LOL-000')
+  const result = action.filterIssueIds('PAC-000,RE-123,,LOL-333,LOL-000')
   const anotherResult = action.filterIssueIds('LOL-000')
 
   console.log(result)
