9612706ee1c538ce7daf50b6e3be788debe0e897
Sören Mothes <soeren@staffbase.com>
2022-03-03 21:01:39 +0100

    add ARN webhook call to inform the Product Enablement team

---
+++ arn.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+class Arn {
+  constructor ({ webhookUrl }) {
+    this.webhookUrl = webhookUrl || '';
+  }
+
+  async callWebhook(labelValue) {
+    const url = `${this.webhookUrl}?labelName=${labelValue}`
+    console.log(`Call webhook ${url}`);
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {},
+    });
+
+    if (!response.ok) {
+      const errorMsg = await response.text();
+
+      throw new Error(errorMsg)
+    }
+  }
+}
+
+module.exports = Arn;
